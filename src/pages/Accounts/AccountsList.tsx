940b8d7969e436cc8130e358177949b4efb96cb8
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2023-07-15 19:08:45 -0300

    implementada chamada para listar todas as contas no useEffect

--- src/pages/Accounts/AccountsList.tsx
+++ src/pages/Accounts/AccountsList.tsx
@@ -1,3 +1,63 @@
+import {
+  Table,
+  TableCaption,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+interface AccountType {
+  _id: string;
+  descricao: string;
+  estaPaga: boolean;
+  valor: number;
+  dataVencimento: string;
+  tipo: "Despesa" | "Receita";
+}
+
 export function AccountsList() {
-  return <>página de listagem de contas</>;
+  const [accounts, setAccounts] = useState<AccountType[]>([]);
+
+  async function callRequest() {
+    const response = await axios.get("http://localhost:3000/conta");
+    console.log("response :>> ", response);
+    setAccounts(response.data);
+  }
+
+  useEffect(() => {
+    callRequest();
+  }, []);
+
+  return (
+    <TableContainer>
+      <Table variant="simple">
+        <TableCaption>Listagem de Contas Cadastradas</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>DESCRIÇÃO</Th>
+            <Th isNumeric>VALOR</Th>
+            <Th>TIPO</Th>
+            <Th>PAGA?</Th>
+            <Th>VENCIMENTO</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {accounts.map((account) => (
+            <Tr>
+              <Td>{account.descricao}</Td>
+              <Td>{account.valor}</Td>
+              <Td>{account.tipo}</Td>
+              <Td>{account.estaPaga ? "Sim" : "Não"}</Td>
+              <Td>{account.dataVencimento}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
